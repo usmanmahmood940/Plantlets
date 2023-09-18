322127fa9ab4a9970acd2ad2f2584ca3c7b53d80
Usman Mahmood <usmanmahmood940@gmail.com>
2023-09-19 04:41:09 +0500

    Added toggle user store signup

--- app/src/main/java/com/example/plantlets/SignupActivity.kt
+++ app/src/main/java/com/example/plantlets/SignupActivity.kt
@@ -2,6 +2,7 @@ package com.example.plantlets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.plantlets.databinding.ActivitySignupBinding
 
 private lateinit var binding: ActivitySignupBinding
@@ -11,6 +12,19 @@ class SignupActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.rgType.setOnCheckedChangeListener { group, checkedId ->
+            // Handle the selected radio button change here
+            when (checkedId) {
+                R.id.rb_user -> {
+                  binding.rlStore.visibility = View.GONE
+                }
+                R.id.rb_vendor -> {
+                    binding.rlStore.visibility = View.VISIBLE
+                }
+            }
+        }
 
         when(binding.rgType.checkedRadioButtonId){
             R.id.rb_user ->{
