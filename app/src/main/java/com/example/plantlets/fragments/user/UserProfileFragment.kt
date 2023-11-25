a1f0ccbad89e3f67decd69eb74bdbc33fb2863df
bluwhalee <ahmadaliravian@outlook.com>
2023-11-25 23:10:11 +0500

    admin panel 3

--- app/src/main/java/com/example/plantlets/fragments/user/UserProfileFragment.kt
+++ app/src/main/java/com/example/plantlets/fragments/user/UserProfileFragment.kt
@@ -5,25 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.plantlets.R
+import com.example.plantlets.databinding.FragmentUserProfileBinding
+import com.example.plantlets.models.User
+import com.example.plantlets.repositories.LocalRepository
+import dagger.hilt.android.AndroidEntryPoint
+import java.lang.RuntimeException
+import javax.inject.Inject
 
 
+@AndroidEntryPoint
 class UserProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private lateinit var binding : FragmentUserProfileBinding
+    @Inject
+    lateinit var localRepository: LocalRepository
+    private  var user : User? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+
+        binding = FragmentUserProfileBinding.inflate(layoutInflater)
+
+        setUI()
+
+        return binding.root
+    }
+
+    private fun setUI() {
+
+        user = localRepository.getCurrentUserData()
+        binding.apply {
+            etName.text = user?.name
+            etEmail.text = user?.email
+            etMobileNum.text = user?.mobileNumber
+            Glide.with(ivProfileImage).load(user?.image).into(ivProfileImage)
+        }
     }
 
 
